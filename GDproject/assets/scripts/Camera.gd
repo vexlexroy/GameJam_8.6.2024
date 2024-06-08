f248f28caea64732b4157fab9e4feb7f7b0cc455
Dlakovuk <ian.dlakovuk@yahoo.com>
2024-06-08 12:52:21 +0200

    pen falls

--- GDproject/assets/scripts/Camera.gd
+++ GDproject/assets/scripts/Camera.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var target  : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	self.position.x = target.position.x;
+	self.position.y = target.position.y;
 	pass
--- GDproject/assets/scripts/Character_Controller.gd
+++ GDproject/assets/scripts/Character_Controller.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var speed : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
